de8451c9dd2df3ec61087df40b5ef826e70f808a
josephkem <josephkemoy@gmail.com>
2021-10-27 13:14:50 -0400

    Added box and grid component inside the users component along with some styling

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,8 +2,18 @@ import React, { useState, useEffect } from "react";
 import { Box, Grid, CircularProgress } from "@material-ui/core";
 import axios from "axios";
 import { makeStyles } from "@material-ui/core";
+import { mergeClasses } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  usersContainer: {
+    textAlign: "center",
+    padding: "80px 10px 0px 10px",
+    backgroundColor: "rgb(68, 68, 68)",
+  },
+}));
 
 const Users = () => {
+  const classes = useStyles();
   const [users, setUsers] = useState(null);
   useEffect(() => {
     axios.get("https://reqres.in/api/users?page=1").then((res) => {
@@ -18,9 +28,9 @@ const Users = () => {
 
   console.log(users);
   return (
-    <div>
-      <h1>testing</h1>
-    </div>
+    <Box>
+      <Grid container spacing={2} className={classes.usersContainer}></Grid>
+    </Box>
   );
 };
 
