ddfd00506684765c1c22ff6cec442497dd9549e0
josephkem <josephkemoy@gmail.com>
2021-10-27 12:56:27 -0400

    Used axios to fetch the data from the api and imported the Users component into the main app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import Users from "./components/Users";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <h1>Hello</h1>
+      <Users />
     </div>
   );
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -5,6 +5,11 @@ import { makeStyles } from "@material-ui/core";
 
 const Users = () => {
   const [users, setUsers] = useState(null);
+  useEffect(() => {
+    axios
+      .get("https://reqres.in/api/users?page=1")
+      .then((res) => console.log(res.data.data));
+  }, []);
   return (
     <div>
       <h1>testing</h1>
