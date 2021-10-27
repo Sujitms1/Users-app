dfe86c10212d74cadeab07b9a1077a45f4c5e2f9
josephkem <josephkemoy@gmail.com>
2021-10-27 12:44:49 -0400

    Imported components to build the users component and get the data

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Box, Grid, CircularProgress } from "@material-ui/core";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core";
 
 const Users = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>testing</h1>
+    </div>
+  );
 };
 
 export default Users;
