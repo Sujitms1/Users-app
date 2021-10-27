968c03b64fe39e2d2645c7304b81e25e9bfebccf
josephkem <josephkemoy@gmail.com>
2021-10-27 12:34:09 -0400

    Used material components along with the useStyle hook to build the Navabar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: "black",
+  },
+}));
 
 const Navbar = () => {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <AppBar className={classes.appBar}>
+      <Toolbar>
+        <Typography>Brand Name</Typography>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
