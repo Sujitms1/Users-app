94ca41a8675c7156af0adb4410f7acd2413a7226
josephkem <josephkemoy@gmail.com>
2021-10-27 13:07:36 -0400

    Created an array to transfer the data to the users state from the http repsonse

--- src/components/Users.js
+++ src/components/Users.js
@@ -6,10 +6,17 @@ import { makeStyles } from "@material-ui/core";
 const Users = () => {
   const [users, setUsers] = useState(null);
   useEffect(() => {
-    axios
-      .get("https://reqres.in/api/users?page=1")
-      .then((res) => console.log(res.data.data));
+    axios.get("https://reqres.in/api/users?page=1").then((res) => {
+      const results = res.data.data;
+
+      let userData = [];
+
+      userData.push(results);
+      setUsers(userData);
+    });
   }, []);
+
+  console.log(users);
   return (
     <div>
       <h1>testing</h1>
