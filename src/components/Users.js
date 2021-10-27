8fc10d5fc9bb6225cb79a8bcbb4badd138927c27
josephkem <josephkemoy@gmail.com>
2021-10-27 12:49:19 -0400

    Created useState hook to store the state of the users in the component

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Grid, CircularProgress } from "@material-ui/core";
 import axios from "axios";
 import { makeStyles } from "@material-ui/core";
 
 const Users = () => {
+  const [users, setUsers] = useState(null);
   return (
     <div>
       <h1>testing</h1>
