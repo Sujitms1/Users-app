49524ecff874f2829f01a6a322238d836c58d827
josephkem <josephkemoy@gmail.com>
2021-10-27 13:26:11 -0400

    Imported component to buid the user card

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,4 +1,12 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
 const UserDetails = () => {
   return <div></div>;
